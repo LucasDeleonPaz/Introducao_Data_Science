6d8a3ca07fabcf0644d74d47f332d3726958d715
LucasDeleonPaz <lucasdeleonpaz@gmail.com>
2022-09-17 17:36:00 -0300

    Feat|Introdução ao Python - Conhecendo os dados iniciais da linguagem

---
+++ CONHECENDO_A_LINGUAGEM/FUNCOES_ENTRADA_SAIDA/funcao.py
@@ -0,0 +1,6 @@
+name_user = input('Digite seu nome: ')
+age_user = input('Digite sua idade: ')
+address_user = input('Digite seu endereço: ')
+
+print(f'Olá {name_user}, idade {age_user}, endereço {address_user}')
+
